1189f3d8f4d7739406c40717032ee08757f27127
Nirmala <nirmala.talari@gmail.com>
2019-03-01 13:33:20 -0500

    DuplicateDeleteIntegers

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -10,24 +10,37 @@ import java.util.*;
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     private Integer[] intArray;
     private DuplicateDeleter<Integer> deleter;
-    Map<Integer, Integer> map = new HashMap();
-    ArrayList<Integer> nums = new ArrayList<>();
+
 
     public IntegerDuplicateDeleter(Integer[] intArray) {
         super(intArray);
     }
 
     @Override
-    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+    public Integer[] removeDuplicates(int leastNumberOfDuplications) {
         Map<Integer, Integer> dupMap = new HashMap();
-        dupMap = getDuplicatesMap(maxNumberOfDuplications);
+        dupMap = getDuplicatesMap(leastNumberOfDuplications);
+        ArrayList<Integer> nums = new ArrayList<>();
 
+        for (Map.Entry<Integer, Integer> entry : dupMap.entrySet()) {
+            int i = 0;
+            if (entry.getValue() < (leastNumberOfDuplications)) {
+                while (i < entry.getValue()) {
+                    nums.add(entry.getKey());
+                    i++;
+                }
+            }
+        }
+        Integer[] arr = new Integer[nums.size()];
+        arr = nums.toArray(arr);
 
-        return new Integer[0];
+        return arr;
     }
 
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        Map<Integer, Integer> map = new HashMap();
+        ArrayList<Integer> nums = new ArrayList<>();
         Map<Integer, Integer> dupMap = new HashMap();
         dupMap = getDuplicatesMap(exactNumberOfDuplications);
         for (Map.Entry<Integer, Integer> entry : dupMap.entrySet()) {
@@ -47,6 +60,7 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     }
 
     public Map<Integer, Integer> getDuplicatesMap(int numberOfDuplications) {
+        Map<Integer, Integer> map = new HashMap();
         for (Integer num : array) {
             Integer count = map.get(num);
             if (count == null) {
