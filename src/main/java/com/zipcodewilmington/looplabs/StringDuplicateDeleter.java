c0908ff96bfb1002d467e988575bdaf04714bb57
Nirmala <nirmala.talari@gmail.com>
2019-03-02 13:26:47 -0500

    StringDupl

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,7 +1,10 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/28/18.
+ *
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
@@ -12,11 +15,57 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        Map<String, Integer> map = new HashMap();
+        map = getDupStrMap(maxNumberOfDuplications);
+        ArrayList<String> arrList = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            Integer j = 0;
+            if (entry.getValue() < maxNumberOfDuplications) {
+                while (j<entry.getValue()) {
+                    arrList.add(entry.getKey());
+                    j++;
+                }
+            }
+        }
+        String[] arrRet = new String[arrList.size()];
+        arrRet = arrList.toArray(arrRet);
+
+        return arrRet;
     }
+
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        Map<String, Integer> map = new HashMap();
+        map = getDupStrMap(exactNumberOfDuplications);
+        ArrayList<String> arrList = new ArrayList<>();
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            Integer j = 0;
+            if (entry.getValue() != exactNumberOfDuplications) {
+                while (j<entry.getValue()) {
+                    arrList.add(entry.getKey());
+                    j++;
+                }
+            }
+        }
+        String[] arrRet = new String[arrList.size()];
+        arrRet = arrList.toArray(arrRet);
+
+        return arrRet;
+    }
+
+    public Map<String, Integer> getDupStrMap(int dupCount){
+        Map<String, Integer> duMap = new HashMap();
+        for(String str: array){
+            Integer count = duMap.get(str);
+            if(count == null)
+                duMap.put(str,1);
+            else
+                duMap.put(str,++count);
+        }
+        return duMap;
     }
 }
+
